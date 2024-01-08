34a4b190673f44782016bef3d536d13bf327c0d0
SAMIR <samir.nanani@0xblock.co>
2024-01-08 16:49:39 +0530

    add some feature

--- src/firebase/Setup.jsx
+++ src/firebase/Setup.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8J49xCvqoNr6bq-kMH10IknFGBELb5og",
-  authDomain: "sample-app-3a98b.firebaseapp.com",
-  projectId: "sample-app-3a98b",
-  storageBucket: "sample-app-3a98b.appspot.com",
-  messagingSenderId: "136889459862",
-  appId: "1:136889459862:web:2d296d608e92ad47d8ceb7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
